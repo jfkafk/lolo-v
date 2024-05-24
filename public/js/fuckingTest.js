771b5208aa3e09931932192527c6bcc2530ebf95
jfkafk <karl.tamm15@outlook.com>
2024-05-24 14:21:33 +0300

    initial

--- public/js/fuckingTest.js
+++ public/js/fuckingTest.js
@@ -1,10 +1,10 @@
-import RSSParser from 'https://npmjs.org/package/rss-parser';
+import RSSParser from 'rss-parser';
 
 let parser = new RSSParser({
     customFields: {
         // to fetch media:content along with image.
-        item:[
-            ['media:content', "media"]
+        item: [
+            [ 'media:content', "media" ]
         ]
     }
 });
@@ -12,9 +12,19 @@ let parser = new RSSParser({
 export const parseRssFeedAndCreateList = async () => {
     try {
         // Parse the RSS feed URL
-        const feed = await parser.parseURL('https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss');
+        // Proxy to get around cors policy.
+        const CORS_PROXY = "";
 
-        // Extract and convert each item to JSON.
+        // Construct the final URL to fetch
+        const finalUrl = CORS_PROXY + 'https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss';
+
+        // Fetch the RSS feed through the proxy
+        await parser.parseURL(finalUrl, function (err, feed) {
+            console.log(feed);
+            feed.items.forEach(function(entry) {
+                console.log(entry.title + ':' + entry.link);
+            });
+        });
         const itemList = feed.items.map(item => ({
             title: item.title,
             link: item.link,
