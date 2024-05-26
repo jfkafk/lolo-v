ac5f9cb626c0906ebbac30aa22d0b28408d1a402
jfkafk <karl.tamm15@outlook.com>
2024-05-26 20:56:42 +0300

    page keeps its content after refresh. categories have different shadows.

--- public/js/interaction.js
+++ public/js/interaction.js
@@ -1,46 +1,73 @@
-// Remove button removes from main feed and adds it to the 'removed' section.
-$('body').on('click', '.remove-button', function () {
-    const closeButtonId = '#close' + $(this).closest('.newsElement').attr('id');
-    const newsElementClone = $(this).closest('.newsElement').clone(true);
-    $('#removed-container').append(newsElementClone);
-    $(closeButtonId).parent().parent().remove(); // Remove the parent div of the clicked button
-});
-$('.removed-news').on('click', function() {
-    this.classList.toggle('toggled'); // toggle animation.
-    $('.read-later-news').removeClass('toggled');
-    $('.liked-news').removeClass('toggled');
-    toggleContainers(getCurrentVisibleContainer(), '#removed-container')
-});
+// Event handlers and interactions.
 
-// Flag button changes the color of the flag and adds it into the 'read later' section.
-$('body').on('click', '.like-button', function () {
-    // Directly use the index to find the corresponding "close" button
-    const newsElementClone = $(this).closest('.newsElement').clone(true);
-    $('#liked-container').append(newsElementClone);
-});
-$('.liked-news').on('click', function() {
-    this.classList.toggle('toggled'); // toggle animation.
-    $('.read-later-news').removeClass('toggled');
-    $('.removed-news').removeClass('toggled');
+// Create functions behind buttons.
+function attachEventHandlers() {
+    // News feed element buttons.
 
-    toggleContainers(getCurrentVisibleContainer(), '#liked-container');
-});
+    // Remove button removes from main feed and adds it to the 'removed' section.
+    $('body').on('click', '.remove-button', function () {
+        const closeButtonId = '#close' + $(this).closest('.newsElement').attr('id');
+        const newsElementClone = $(this).closest('.newsElement').clone(true);
+        $('#removed-container').append(newsElementClone);
+        $(closeButtonId).parent().parent().remove(); // Remove the parent div of the clicked button
+        saveContainerState();
+    });
 
-// Like button changes the color of heart icon and adds it into the 'liked' section.
-$('body').on('click', '.flag-button', function () {
-    // Directly use the index to find the corresponding "close" button
-    const newsElementClone = $(this).closest('.newsElement').clone(true);
-    $('#later-container').append(newsElementClone);
-});
-$('.read-later-news').on('click', function() {
-    this.classList.toggle('toggled'); // toggle animation.
-    $('.liked-news').removeClass('toggled');
-    $('.removed-news').removeClass('toggled');
-    toggleContainers(getCurrentVisibleContainer(), '#later-container');
-});
+    // Flag button changes the color of the flag and adds it into the 'read later' section.
+    $('body').on('click', '.flag-button', function () {
+        // Directly use the index to find the corresponding "close" button
+        const newsElementClone = $(this).closest('.newsElement').clone(true);
+        $('#later-container').append(newsElementClone);
+        saveContainerState();
+    });
 
-// to toggle the visibility of the current and next containers
+    // Like button changes the color of heart icon and adds it into the 'liked' section.
+    $('body').on('click', '.like-button', function () {
+        // Directly use the index to find the corresponding "close" button
+        const newsElementClone = $(this).closest('.newsElement').clone(true);
+        $('#liked-container').append(newsElementClone);
+        saveContainerState();
+    });
+
+    // Header navbar buttons.
+
+    $('.removed-news').on('click', function() {
+        this.classList.toggle('toggled'); // toggle animation.
+
+        // Remove others toggled visuals.
+        $('.read-later-news').removeClass('toggled');
+        $('.liked-news').removeClass('toggled');
+
+        // hide current container and display next.
+        toggleContainers(getCurrentVisibleContainer(), '#removed-container')
+    });
+
+    $('.liked-news').on('click', function() {
+        this.classList.toggle('toggled'); // toggle animation.
+
+        // Remove others toggled visuals.
+        $('.read-later-news').removeClass('toggled');
+        $('.removed-news').removeClass('toggled');
+
+        // hide current container and display next.
+        toggleContainers(getCurrentVisibleContainer(), '#liked-container');
+    });
+
+    $('.read-later-news').on('click', function() {
+        this.classList.toggle('toggled'); // toggle animation.
+
+        // Remove others toggled visuals.
+        $('.liked-news').removeClass('toggled');
+        $('.removed-news').removeClass('toggled');
+
+        // hide current container and display next.
+        toggleContainers(getCurrentVisibleContainer(), '#later-container');
+    });
+}
+
+// hide current container and display new one.
 function toggleContainers(currentContainer, nextContainer) {
+    // clicking twice closes the container and displays the main feed.
     if (currentContainer === nextContainer) {
         $(currentContainer).toggle();
         $('#feed-container').toggle()
@@ -49,6 +76,7 @@ function toggleContainers(currentContainer, nextContainer) {
     $(currentContainer).hide();
     $(nextContainer).toggle();
 }
+
 // returns the current container.
 function getCurrentVisibleContainer() {
     let containers = ['#feed-container', '#removed-container', '#liked-container', '#later-container'];
@@ -60,4 +88,20 @@ function getCurrentVisibleContainer() {
         }
     });
     return visibleContainer;
+}
+
+// Save all containers current state.
+function saveContainerState() {
+    // Clear to avoid duplications.
+    localStorage.clear();
+
+    const newState = {
+        feed: $('#feed-container').html(),
+        removed: $('#removed-container').html(),
+        liked: $('#liked-container').html(),
+        later: $('#later-container').html()
+    };
+
+    // Save current state.
+    localStorage.setItem('containerState', JSON.stringify(newState));
 }
--- public/js/parseBuildRSS.js
+++ public/js/parseBuildRSS.js
@@ -1,57 +1,95 @@
 // Parse json data from web.
 async function fetchAndParseRSSFeed() {
     try {
+        // Fetch data from 'proxy server'.
         const response = await fetch('https://lolo-v5-server.onrender.com');
-        return response.json(); // Return the items array
+        return response.json();
     } catch (error) {
         console.error('Error fetching or parsing RSS feed:', error);
         return [];
     }
 }
-// Create html elements from received json.
+
 $(document).ready(async function () {
     const feed = await fetchAndParseRSSFeed();
-    let feedContent = '';
-    console.log("received " + feed.length + "items")
-    $.each(feed, function (index, item) {
-
-        // Format the date.
-        let formatted = {day: "numeric", month: "long", year: "numeric"};
-        let articleDate = new Date(item.item.pubDate).toLocaleDateString("en-GB", formatted);
-        let newsLink = item.item.link;
-
-        let author = item.item.author || null;
-
-        let imageURL = item.item.media?.['$']?.url;
-
-        // News element.
-        feedContent += '<div id="' + index + '" class="newsElement">';
-        feedContent += '<div class="clickable-div">';
-        feedContent += '<a class="clickable" href=' + newsLink + '>';
-        feedContent += '<img class="newsElement-image" src="' + imageURL + '" alt="display-image">';
-        feedContent += '<h2 class="title">' + item.item.title + '</h2>';
-        feedContent += '<p class="content">' + item.item.content + '</p>';
-        feedContent += '</div>';
-        feedContent += '</a>';
-        if (author != null) feedContent += '<p class="author">' + "by " + item.item.author + '</p>';
-        feedContent += '<p class="pubDate">' + articleDate + '</p>';
-        // Buttons bottom side.
-        feedContent += '<div class="user-options">';
-        feedContent += '<button style="background: none" id="like' + index + '">' +
-            '<img class="like-button" src="./public/assets/heart.svg" alt="heart"' +
-            '</button>'; // liked.
-
-        feedContent += '<button style="background: none"  id="flag' + index + '">' +
-            '<img class="flag-button" src="./public/assets/flag.svg" alt="flag"' +
-            '</button>'; // flagged.
-
-        feedContent += '<button style="background: none"  id="close' + index + '">' +
-            '<img class="remove-button" src="./public/assets/remove.svg" alt="remove">' +
-            '</button>'; // removed.
-        feedContent += '</div>';
-        feedContent += '</div>';
-        feedContent += '</div>';
-    });
-    // Append the generated content to feed container.
-    $('#feed-container').append(feedContent);
+    if (feed.length > 0) {
+        console.log("received " + feed.length + " elements");
+        renderFeed(feed);
+    } else {
+        console.log("No items received");
+    }
 });
+
+// Renders feed. Build new instances if landed on the page for the first time.
+function renderFeed(feed) {
+    // true if page initial loading.
+    if (restoreContainerState()) {
+        let feedContent = '';
+        $.each(feed, function (index, item) {
+            // Format the date.
+            let formatted = {day: "numeric", month: "long", year: "numeric"};
+            let articleDate = new Date(item.item.pubDate).toLocaleDateString("en-GB", formatted);
+            let newsLink = item.item.link;
+            let author = item.item.author || null;
+            let imageURL = item.item.media?.['$']?.url;
+            let category = item.item.categories[0]['_'] || null;
+            console.log(category + index);
+
+            // News element.
+            if (category != null) feedContent += '<div class="'+ category +'">';
+
+            feedContent += `<div id="${ index }" class="newsElement">
+                                <div class="clickable-div">
+                                    <a class="clickable" href="${ newsLink }">
+                                        <img class="newsElement-image" src="${ imageURL }" alt="display-image">
+                                        <h2 class="title">${ item.item.title }</h2>
+                                        <p class="content">${ item.item.content }</p>
+                                    </a>
+                                </div>`;
+
+            if (author) feedContent += `<p class="author">by ${ author }</p>`;
+
+            feedContent += `<p class="pubDate">${ articleDate }</p>
+                            <div class="user-options">
+                                <button style="background: none" id="like${ index }">
+                                    <img src="./public/assets/heart.svg" class="like-button" alt="heart">
+                                </button>
+                                <button style="background: none" id="flag${ index }">
+                                    <img class="flag-button" src="./public/assets/flag.svg" alt="flag">
+                                </button>
+                                <button style="background: none" id="close${ index }">
+                                    <img class="remove-button" src="./public/assets/remove.svg" alt="remove">
+                                </button>
+                            </div>
+                          </div>`;
+            if (category != null) feedContent += '</div>';
+        });
+        // Append the generated content to feed container.
+        $('#feed-container').append(feedContent);
+        console.log("Welcome!");
+    }
+    attachEventHandlers();
+    $('#later-container').hide();
+    $('#liked-container').hide();
+    $('#removed-container').hide();
+}
+function restoreContainerState() {
+    // Returns true if page is loaded first time.
+    let isFirstLoad = true;
+
+    const state = JSON.parse(localStorage.getItem('containerState'));
+    if (state) {
+        ['feed', 'removed', 'liked', 'later'].forEach(containerId => {
+            // Access all keys from the saved data.
+            const savedHtml = state[containerId];
+            if (savedHtml) {
+                if (isFirstLoad) {
+                    isFirstLoad = false;
+                }
+                // Set the innerHTML of the container to its saved state.
+                $('#' + containerId + '-container').html(savedHtml);
+            }
+        });
+    }
+    return isFirstLoad;
+}
